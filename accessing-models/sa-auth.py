16920509a40d08ef6f5bf700129d29c0166e1dfd
Yathartha <varshneya.yathartha@gmail.com>
2026-03-29 09:49:09 +0530

    streaming output

--- accessing-models/sa-auth.py
+++ accessing-models/sa-auth.py
@@ -2,13 +2,13 @@
 
 from google.genai import Client
 from google.oauth2.service_account import Credentials
-api_key_path = "project-99973915-6ec0-4919-a49-b563da086f5e.json"
+api_key_path = "project-99973915-6ec0-4919-a49-4a99af956eb0.json"
 
 credentials = Credentials.from_service_account_file(
     api_key_path, scopes=["https://www.googleapis.com/auth/cloud-platform"]
 )
 
-PROJECT_ID =  "proect-99973915-6ec0-4919-a49"
+PROJECT_ID =  "project-99973915-6ec0-4919-a49"
 REGION = "us-central1"
 MODEL_NAME = "gemini-2.5-flash"
 
@@ -25,4 +25,3 @@ response = client.models.generate_content(
 )
 
 print("Model Response:\n", response.text)
-j
